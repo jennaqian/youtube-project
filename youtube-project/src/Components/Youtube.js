d7f25f9ede93484a421ff21fee5bcd869692a08c
Yesenia L <ylopezcr123@gmail.com>
2021-05-20 12:40:56 -0400

    code

--- youtube-project/src/Components/Youtube.js
+++ youtube-project/src/Components/Youtube.js
@@ -1,5 +1,66 @@
+// import React, { Component } from 'react';
+// import axios from 'axios'
+
+// export default class Youtube extends Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             links: [],
+//             input: "",
+//         }
+//     }
+
+//     componentDidMount() {
+//         this.getVideos();
+//     }
+
+//     getVideos = async () => {
+//         console.log(process.env.REACT_APP_keyAPI)
+//         const {data} = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_keyAPI }`)
+//         this.setState ({
+//             link : data.items[0]
+//         })
+//         console.log(this.state.link)
+//     }
+
+//     // handleChange = (event) => {
+//     //     this.setState({
+//     //         input: event.target.value,
+//     //     })
+//     //     console.log(this.state.input)
+//     // }
+
+//     // handleSubmit = (event) => {
+//     //     event.preventDefault()
+//     //     // this.setState({
+
+//     //     // })
+//     // }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>This is a search</h1>
+
+//                 <form onSubmit={this.handleSubmit}>
+//                     <input type="text" onChange={this.handleChange} className="Input" placeholder="Search here..."/>
+//                     <input type="submit" ></input>
+//                 </form>
+
+//                 <ul>
+
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+// import Video from './Components/Video'
 
 export default class Youtube extends Component {
     constructor() {
@@ -7,6 +68,8 @@ export default class Youtube extends Component {
         this.state = {
             links: [],
             input: "",
+            videos: [],
+            show: true
         }
     }
 
@@ -16,40 +79,63 @@ export default class Youtube extends Component {
 
     getVideos = async () => {
         console.log(process.env.REACT_APP_keyAPI)
-        const {data} = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_keyAPI }`)
+        const {data} = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?videoId&key=${process.env.REACT_APP_keyAPI }`)
         this.setState ({
-            link : data.items[0]
+            videos : data.items[0]
         })
-        console.log(this.state.link)
+        console.log(this.state.videos)
     }
 
-    // handleChange = (event) => {
-    //     this.setState({
-    //         input: event.target.value,
-    //     })
-    //     console.log(this.state.input)
-    // }
+    toggle = () => {
+        this.setState({
+            show : !this.state.show
+        })
+    }
+    handleChange = (event) => {
+        this.setState({
+       [event.target.id] : event.target.value
+        })
+        console.log(this.state.input)
+    }
 
-    // handleSubmit = (event) => {
-    //     event.preventDefault()
-    //     // this.setState({
-
-    //     // })
-    // }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.getVideos(this.state.input)
+        this.setState({
+            input:''
+        })
+    }
 
     render() {
+        const {input} = this.state
         return (
             <div>
                 <h1>This is a search</h1>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={this.handleChange} className="Input" placeholder="Search here..."/>
-                    <input type="submit" ></input>
+                    <label htmlFor="input"></label>
+                    <input type="text" onChange={this.handleChange} placeholder="Search here..." id='input' value={this.state.input}/>
+                    <button onClick= {this.getVideos}>Search</button>
+                    {/* <input type="submit"   /> */}
                 </form>
+            <main>
 
-                <ul>
 
-                </ul>
+
+                {this.state.show && this.state.videos.map((video, i)=> {
+                    return (
+                        <link key={video.id.videoId + i} to={`/video/${video.id.videoId}`}>
+                            <p>
+
+        <img src={input.snippet.thumbnails.default.url} alt={ input.snippet.title }/>
+          <h4>{input.snippet.title}</h4>  
+                            </p>
+                        </link>
+        
+                    )
+                }) 
+            }
+            </main>
             </div>
         )
     }
